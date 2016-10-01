a5b6a9c5929ae33ca52c3f4cd7233f6fde4b25f1
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-10-01 13:51:25 +0800

    [MESG]: 完善自动转换脚本

---
+++ src/lib/mesg/convert.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+INCL_DIR=../../incl/mesg
+mkdir -p $INCL_DIR
+
+LIST="mesg_room.proto"
+LIST=$LIST" mesg_online.proto"
+LIST=$LIST" mesg_online_ack.proto"
+LIST=$LIST" mesg_join_ack.proto"
+
+for ITEM in $LIST;
+do
+    echo ${ITEM}
+    if [ -e ${ITEM} ]; then
+        protoc-c --c_out=. ${ITEM};
+
+        INCL=`echo ${ITEM} | awk -F. '{print $1}'`;
+
+        echo "mv ${INCL}.pb-c.h ${INCL_DIR}";
+        mv "${INCL}.pb-c.h" ${INCL_DIR};
+    fi
+done
