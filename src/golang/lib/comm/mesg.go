3c74f7903b21869ec5e772b6b27987ac9dcb4239
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-10-31 22:02:57 +0800

    [COMM]: 完善通用消息头的处理

--- src/golang/lib/comm/mesg.go
+++ src/golang/lib/comm/mesg.go
@@ -10,6 +10,10 @@ const (
 	MSG_CHKSUM_VAL = 0x1ED23CB4 /* 校验码 */
 )
 
+var (
+	MESG_HEAD_SIZE = binary.Size(MesgHeader{})
+)
+
 /* 通用协议头 */
 type MesgHeader struct {
 	Cmd    uint32 /* 消息类型 */
@@ -21,12 +25,36 @@ type MesgHeader struct {
 	Serial uint64 /* 流水号(注: 全局唯一流水号) */
 }
 
+func (head *MesgHeader) GetCmd() uint32 {
+	return head.Cmd
+}
+
+func (head *MesgHeader) GetLength() uint32 {
+	return head.Length
+}
+
+func (head *MesgHeader) GetChkSum() uint32 {
+	return head.ChkSum
+}
+
+func (head *MesgHeader) GetSid() uint64 {
+	return head.Sid
+}
+
+func (head *MesgHeader) GetNid() uint32 {
+	return head.Nid
+}
+
+func (head *MesgHeader) GetSerial() uint64 {
+	return head.Serial
+}
+
 type MesgPacket struct {
 	buff []byte /* 接收数据 */
 }
 
 /* "主机->网络"字节序 */
-func mesg_head_hton(header *MesgHeader, p *MesgPacket) {
+func MesgHeadHton(header *MesgHeader, p *MesgPacket) {
 	binary.BigEndian.PutUint32(p.buff[0:4], header.Cmd)      /* CMD */
 	binary.BigEndian.PutUint32(p.buff[4:8], header.Flag)     /* FLAG */
 	binary.BigEndian.PutUint32(p.buff[8:12], header.Length)  /* LENGTH */
@@ -37,16 +65,16 @@ func mesg_head_hton(header *MesgHeader, p *MesgPacket) {
 }
 
 /* "网络->主机"字节序 */
-func mesg_head_ntoh(p *MesgPacket) *MesgHeader {
+func MesgHeadNtoh(data []byte) *MesgHeader {
 	head := &MesgHeader{}
 
-	head.Cmd = binary.BigEndian.Uint32(p.buff[0:4])
-	head.Flag = binary.BigEndian.Uint32(p.buff[4:8])
-	head.Length = binary.BigEndian.Uint32(p.buff[8:12])
-	head.ChkSum = binary.BigEndian.Uint32(p.buff[12:16])
-	head.Sid = binary.BigEndian.Uint64(p.buff[16:24])
-	head.Nid = binary.BigEndian.Uint32(p.buff[24:28])
-	head.Serial = binary.BigEndian.Uint64(p.buff[28:36])
+	head.Cmd = binary.BigEndian.Uint32(data[0:4])
+	head.Flag = binary.BigEndian.Uint32(data[4:8])
+	head.Length = binary.BigEndian.Uint32(data[8:12])
+	head.ChkSum = binary.BigEndian.Uint32(data[12:16])
+	head.Sid = binary.BigEndian.Uint64(data[16:24])
+	head.Nid = binary.BigEndian.Uint32(data[24:28])
+	head.Serial = binary.BigEndian.Uint64(data[28:36])
 
 	return head
 }
