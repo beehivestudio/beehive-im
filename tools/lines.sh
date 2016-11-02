41c709fae0406625aa03ab68bde7194a26a87ade
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-11-02 11:27:31 +0800

    [TOOLS]: 完善代码行统计脚本

--- tools/lines.sh
+++ tools/lines.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
 # 设置统计目录
-DIR="../src"
+DIR="../src/clang"
 SRC="$DIR/exec"
-SRC="$SRC $DIR/demo"
 SRC="$SRC $DIR/incl"
 SRC="$SRC $DIR/lib/"
+DIR="../src/golang"
+SRC="$SRC $DIR/exec"
+SRC="$SRC $DIR/demo"
+SRC="$SRC $DIR/lib/"
+
+
+
 
 # 总行数
-total=`find $SRC -name "*.h" -o -name "*.c" | xargs cat | wc -l`
+total=`find $SRC -name "*.h" -o -name "*.c" -o -name "*.go" | xargs cat | wc -l`
 
 # 空行数
-empty=`find $SRC -name "*.h" -o -name "*.c" | xargs grep "^[[:space:]]\{0,\}$" | wc -l`
+empty=`find $SRC -name "*.h" -o -name "*.c" -o -name "*.go" | xargs grep "^[[:space:]]\{0,\}$" | wc -l`
 
 # 注释数
-note=`find $SRC -name "*.h" -o -name "*.c" | xargs grep \
+note=`find $SRC -name "*.h" -o -name "*.c" -o -name "*.go" | xargs grep \
      -e "^[[:space:]]\{0,\}/\*" \
      -e "^[[:space:]]\{0,\}\*" \
      -e "^[[:space:]]\{0,\}\*\*" \
