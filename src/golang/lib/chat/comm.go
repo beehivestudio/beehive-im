5ab8f6041e80aa12d5ca26ebae0202cc2f13177a
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-11-30 00:04:07 +0800

    [CHAT]: 添加CHAT业务复用功能

---
+++ src/golang/lib/chat/comm.go
@@ -0,0 +1,39 @@
+package chat
+
+import (
+	"errors"
+
+	"github.com/garyburd/redigo/redis"
+
+	"chat/src/golang/lib/comm"
+)
+
+/******************************************************************************
+ **函数名称: AllocSid
+ **功    能: 申请会话SID
+ **输入参数:
+ **     pool: Redis连接池
+ **输出参数: NONE
+ **返    回:
+ **     sid: 会话SID
+ **     err: 日志对象
+ **实现描述:
+ **注意事项:
+ **作    者: # Qifeng.zou # 2016.11.02 10:48:40 #
+ ******************************************************************************/
+func AllocSid(pool *redis.Pool) (sid uint64, err error) {
+	rds := pool.Get()
+	defer rds.Close()
+
+	for {
+		sid, err := redis.Uint64(rds.Do("INCRBY", comm.CHAT_KEY_SID_INCR, 1))
+		if nil != err {
+			return 0, err
+		} else if 0 == sid {
+			continue
+		}
+		return sid, nil
+	}
+
+	return 0, errors.New("Alloc sid failed!")
+}
