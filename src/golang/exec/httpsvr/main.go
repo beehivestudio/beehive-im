c47df77963afbfc3007d6433bcfe37cca16e4597
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-12-06 17:37:57 +0800

    [HTTPSVR]: 整理代码 增强代码可读性

--- src/golang/exec/httpsvr/main.go
+++ src/golang/exec/httpsvr/main.go
@@ -11,8 +11,8 @@ import (
 	"chat/src/golang/exec/httpsvr/routers"
 )
 
-/* 设置配置信息 */
-func set_conf(conf *controllers.HttpSvrConf) {
+/* 设置BEEGO配置 */
+func init_bconf(conf *controllers.HttpSvrConf) {
 	beego.BConfig.AppName = "beehive-im"
 	beego.BConfig.Listen.EnableHTTP = true
 	beego.BConfig.Listen.HTTPAddr = ""
@@ -20,7 +20,8 @@ func set_conf(conf *controllers.HttpSvrConf) {
 	beego.BConfig.RouterCaseSensitive = true
 }
 
-func main() {
+/* 初始化 */
+func _init() *controllers.HttpSvrCntx {
 	var conf controllers.HttpSvrConf
 
 	flag.Parse()
@@ -30,22 +31,45 @@ func main() {
 	/* > 加载HTTPSVR配置 */
 	if err := conf.LoadConf(); nil != err {
 		fmt.Printf("Load configuration failed! errmsg:%s\n", err.Error())
-		return
+		return nil
 	}
+
+	init_bconf(&conf)
 
 	/* > 初始化HTTPSVR环境 */
 	ctx, err := controllers.HttpSvrInit(&conf)
 	if nil != err {
 		fmt.Printf("Initialize context failed! errmsg:%s\n", err.Error())
+		return nil
+	}
+
+	return ctx
+}
+
+/* 注册回调 */
+func register(ctx *controllers.HttpSvrCntx) {
+	ctx.Register()
+	routers.Router()
+}
+
+/* 启动服务 */
+func launch(ctx *controllers.HttpSvrCntx) {
+	ctx.Launch()
+	beego.Run()
+}
+
+/* 主函数 */
+func main() {
+	/* > 初始化 */
+	ctx := _init()
+	if nil == ctx {
+		fmt.Printf("Initialize context failed!\n")
 		return
 	}
 
-	/* > 启动HTTPSVR服务 */
-	ctx.Launch()
+	/* > 注册回调 */
+	register(ctx)
 
-	routers.Router()
-
-	set_conf(&conf)
-
-	beego.Run()
+	/* > 启动服务 */
+	launch(ctx)
 }
