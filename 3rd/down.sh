4616612a6ba9fbceb28986d27f140491fc3f6564
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-03-10 16:50:09 +0800

    [3RD]: 方便第三方库的下载

--- 3rd/down.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-go get github.com/gorilla/websocket
-go get github.com/astaxie/beego/logs
-go get github.com/golang/protobuf/proto
-go get github.com/garyburd/redigo/redis
---
+++ 3rd/download.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+root="$GOPATH/src"
+
+LIBS[0]="github.com/gorilla/websocket"
+LIBS[1]="github.com/astaxie/beego/logs"
+LIBS[2]="github.com/golang/protobuf/proto"
+LIBS[3]="github.com/garyburd/redigo/redis"
+
+echo ${LIBS[*]}
+
+for item in ${LIBS[*]}
+do
+    if [ -f "$root/$item" ]; then
+        echo "go get $item"
+        go get $item
+    else
+        echo "$root/$item exists!"
+    fi
+done
+
