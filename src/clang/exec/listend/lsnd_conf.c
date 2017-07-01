36b38d5670fd0953f649eff937faf1e3e1fa5520
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-07-01 12:51:57 +0800

    [LISTEND]: 完善群组ID配置的解析处理

--- src/clang/exec/listend/lsnd_conf.c
+++ src/clang/exec/listend/lsnd_conf.c
@@ -95,10 +95,19 @@ static int lsnd_conf_load_comm(xml_tree_t *xml, lsnd_conf_t *conf, log_cycle_t *
     xml_node_t *node, *fix;
 
     /* > 加载结点ID */
+    node = xml_query(xml, ".LISTEND.GID");
+    if (NULL == node
+        || 0 == node->value.len) {
+        log_error(log, "Get node gid failed!");
+        return -1;
+    }
+
+    conf->gid = str_to_num(node->value.str);
+
+    /* > 加载结点ID */
     node = xml_query(xml, ".LISTEND.ID");
     if (NULL == node
-        || 0 == node->value.len)
-    {
+        || 0 == node->value.len) {
         log_error(log, "Get node id failed!");
         return -1;
     }
@@ -108,8 +117,7 @@ static int lsnd_conf_load_comm(xml_tree_t *xml, lsnd_conf_t *conf, log_cycle_t *
     /* > 加载工作路径 */
     node = xml_query(xml, ".LISTEND.WORKDIR");
     if (NULL == node
-        || 0 == node->value.len)
-    {
+        || 0 == node->value.len) {
         log_error(log, "Get work directory failed!");
         return -1;
     }
@@ -380,6 +388,7 @@ static int lsnd_conf_load_frwder(xml_tree_t *xml, lsnd_conf_t *lcf, log_cycle_t 
 
     /* > 设置结点ID */
     conf->nid = lcf->nid;
+    conf->gid = lcf->gid;
     snprintf(conf->path, sizeof(conf->path), "%s", lcf->wdir);
 
     /* > 服务端IP */
